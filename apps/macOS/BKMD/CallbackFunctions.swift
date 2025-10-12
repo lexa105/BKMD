ccb1205a29bc04bc5ef4936004c19f4ccf371a14
LexaTuan-macbook <lexatuan@gmail.com>
2025-10-12 19:06:43 +0200

    get rid of the IntMax32, now only prints the elem Usage

--- apps/macOS/BKMD/CallbackFunctions.swift
+++ apps/macOS/BKMD/CallbackFunctions.swift
@@ -1,6 +1,6 @@
 //
-//  CallbackFunctions.swift
-//  BKMD
+//  CallbackFunctions.swift 
+//
 //
 //  Created by MacBook on 29.09.2025.
 //
@@ -10,30 +10,42 @@ import Cocoa
 
 class CallbackFunctions {
     
-    static let HandleIOHIDInputValueCallback: IOHIDValueCallback = { context, result, sender, device in
+    static let HandleIOHIDInputValueCallback: IOHIDValueCallback = { context, result, sender, value in
         
         let monitor = Unmanaged<KeyboardMonitor>
             .fromOpaque(context!)
             .takeUnretainedValue()
         
-        let elem: IOHIDElement = IOHIDValueGetElement(device)
-//        print(elem)
-        let isDown = IOHIDValueGetIntegerValue(device) != 0 //Returns 0 released and 1 pressed
-        let scancode = Int(IOHIDElementGetUsage(elem))
+        let elem: IOHIDElement = IOHIDValueGetElement(value)
         
+        //Filtration only keep the real keyboard usages
+        //AFTER Mouse Update will change.
+        guard IOHIDElementGetUsagePage(elem) == kHIDPage_KeyboardOrKeypad else
+        { return }
+        
+        let isDown = IOHIDValueGetIntegerValue(value) != 0 //Returns 0 released and 1 pressed
+        let rawElemUsage = IOHIDElementGetUsage(elem) //Raw decimal usage of the element
+        
+        //Some random UInt32.max number which is giving
+        guard rawElemUsage != 0, rawElemUsage != UInt32.max else { return }
+        let scanCode = Int(rawElemUsage)
+                
         if isDown {
             //Temporary debug variable
-            let pressedKeys = monitor.pressedKeys.insert(scancode)
+            let pressedKeys = monitor.pressedKeys.insert(scanCode)
             if pressedKeys.inserted {
-                monitor.pressedKeySequence.append(scancode)
+                monitor.pressedKeySequence.append(scanCode)
                 print(monitor.pressedKeys)
             }
         } else {
-            monitor.pressedKeys.remove(scancode)
-            monitor.pressedKeySequence.removeAll { $0 == scancode }
+            monitor.pressedKeys.remove(scanCode)
+            monitor.pressedKeySequence.removeAll { $0 == scanCode }
             
         }
-        
     }
+    
+    
+    
+    
 }
 
